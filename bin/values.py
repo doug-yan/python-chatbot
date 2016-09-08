c8ee2575be0d06651063c15c8610be11cd2e0dc5
Douglas Yan <douglasyan@Douglass-MacBook-Pro.local>
2016-09-08 15:06:25 -0700

    More work

--- bin/values.py
+++ bin/values.py
@@ -1,5 +1,11 @@
 from thesaurus import thesaurus as get_syn
 
 def getValues(entity, values):
-	print 'here is the entity: ' + entity
-	print 'here in values!'
+	value = raw_input("Enter a value for your entity ('q' to quit): ")
+	while value != 'q':
+		synonyms = get_syn(value)
+		print "Here are the synonyms for " + value + ": "
+		for i in range(0, len(synonyms)):
+			print synonyms[i]
+
+		value = raw_input("Enter a value for your entity ('q' to quit): ")
